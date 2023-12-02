e0ef7199c8f1bed67ed5e0007774244326fc5b36
ManuTrindade01 <manuifpr@gmail.com>
2023-12-02 15:03:02 -0300

    validação no alterarReserva

--- alterarReserva.php
+++ alterarReserva.php
@@ -14,7 +14,7 @@ if (isset($_POST['salvar'])) {
     $id_quarto = $_POST['id_quarto'];
     $dataEntrada = $_POST['dataEntrada'];
     $dataSaida = $_POST['dataSaida'];
-    $valorTotalReserva = $_POST['valorTotalReserva'];
+    $valorTotalReserva = str_replace(',', '.', $_POST['valorTotalReserva']);
     /*
     $valorTotalReserva = floatval($valorTotalReservaText);
     $valorTotalReserva = number_ormat($valorTotalReserva, 2, '.', ''); */
@@ -34,17 +34,15 @@ if (isset($_POST['salvar'])) {
     } else {
         */
     // Verificar se o status não é finalizado e se a data atual não é posterior à data de saída
-    if (($status == 4 || $status == 3) && $dataAtual < $dataSaida) {
+    if (($status == 1 || $status == 2) && $dataAtual >= $dataEntrada) {
+        $mensagemErro = "Data de hoje é posterior à data de Entrada.";
+    }
+    if (($status == 4) && $dataAtual < $dataSaida) {
         //$status = 2; // Manter como "Em andamento"
         $mensagemErro = "Data de hoje não é posterior à data de saída.";
     }
-    
+
     // Restrição para evitar atualizar o status para "Finalizado" se a data atual for anterior à data de saída
-    if (($status == 3 || $status == 4) && $dataAtual < $dataSaida) {
-        // Define como "Em andamento" se o status for finalizado e a data atual for anterior à data de saída
-        //$status = 2;
-        $mensagemErro = "Data de hoje não pode ser anterior à data de saída.";
-    }
 
     if (!isset($mensagemErro)) {
         // Preparar a SQL para inserir os dados da reserva
@@ -74,7 +72,19 @@ $linha = mysqli_fetch_array($resultado);
 
 
 <?php require_once("cabecalho.php"); ?>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" href="barbara.ico" type="image/x-icon">
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<script src="https://kit.fontawesome.com/b78968e6be.js" crossorigin="anonymous"></script>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Slab:ital@1&family=Sorts+Mill+Goudy&family=Unna:ital@1&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css" integrity="sha384-QYIZto+st3yW+o8+5OHfT6S482Zsvz2WfOzpFSXMF9zqeLcFV0/wlZpMtyFcZALm" crossorigin="anonymous">
+<link href="css/style.css" rel="stylesheet">
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -108,7 +118,7 @@ $linha = mysqli_fetch_array($resultado);
                     </div>
                 <?php } ?>
 
-                <form method="post" action="alterarReserva.php?id=<?= $linha['id'] ?>">
+                <form method="post" action="alterarReserva.php?id=<?= $linha['id'] ?>" id="form" name="form" onsubmit="return validaForm(form)">
                     <input type="hidden" name="id" value="<?= $linha['id'] ?>">
 
 
@@ -122,7 +132,7 @@ $linha = mysqli_fetch_array($resultado);
                                 $sql = "select * from hospede order by nome";
                                 $resultado = mysqli_query($conexao, $sql);
 
-                                while ($linhaTU = mysqli_fetch_array($resultado)):
+                                while ($linhaTU = mysqli_fetch_array($resultado)) :
                                     $id = $linhaTU['id'];
                                     $nome = $linhaTU['nome'];
 
@@ -137,12 +147,11 @@ $linha = mysqli_fetch_array($resultado);
 
                         <div class="mb-3 col-md">
                             <label for="dataEntrada" class="form-label">Data da Entrada:</label>
-                            <input type="date" class="form-control" name="dataEntrada"
-                                value="<?= $linha['dataEntrada'] ?>">
+                            <input type="date" class="form-control" name="dataEntrada" id="dataEntrada" value="<?= $linha['dataEntrada'] ?>" min="<?php echo date('Y-m-d') ?>">
                         </div>
                         <div class="mb-3 col-md">
                             <label for="dataSaida" class="form-label">Data da Saída:</label>
-                            <input type="date" class="form-control" name="dataSaida" value="<?= $linha['dataSaida'] ?>">
+                            <input type="date" class="form-control" name="dataSaida" id="dataSaida" value="<?= $linha['dataSaida'] ?>" min="<?php echo date('Y-m-d') ?>">
                         </div>
                     </div>
 
@@ -164,7 +173,7 @@ $linha = mysqli_fetch_array($resultado);
                             order by quarto.numero";
                                 $resultado = mysqli_query($conexao, $sql);
 
-                                while ($linhaTU = mysqli_fetch_array($resultado)):
+                                while ($linhaTU = mysqli_fetch_array($resultado)) :
                                     $id = $linhaTU['id'];
                                     $numero = $linhaTU['numero'];
 
@@ -179,9 +188,8 @@ $linha = mysqli_fetch_array($resultado);
 
                         <div class="mb-3 col-md">
                             <label for="valorTotalReserva" class="form-label">Valor Reserva:</label>
-                           
-                            <input type="text" class="form-control" name="valorTotalReserva" id="valorReserva"
-                            value="<?= $linha['valorTotalReserva'] ?>">
+
+                            <input type="text" class="form-control" name="valorTotalReserva" id="valorReserva" value="<?= $linha['valorTotalReserva'] ?>">
                         </div>
 
                         <div class="mb-3 col-md">
@@ -195,43 +203,58 @@ $linha = mysqli_fetch_array($resultado);
                             </select>
                         </div>
 
-                    <div class="mb-3 col-md">
-                        <label for="observacao" class="form-label">Observação:</label>
-                        <input class="form-control" id="observacao" name="observacao"
-                            value="<?= $linha['observacao'] ?>">
-                    </div>
+                        <div class="mb-3 col-md">
+                            <label for="observacao" class="form-label">Observação:</label>
+                            <input class="form-control" id="observacao" name="observacao" value="<?= $linha['observacao'] ?>">
+                        </div>
                     </div>
 
-                    <button name="salvar" type="submit" class="btn"
-                        style="background-color: #a70162; color: #fff;">Salvar
+                    <button name="salvar" type="submit" class="btn" style="background-color: #a70162; color: #fff;">Salvar
                         <i class="fa-solid fa-check"></i>
                     </button>
-                    <a href="controlarReserva.php" type="submit" class="btn btn-warning"><i
-                            class="fa-solid fa-rotate-left"></i></i>
+                    <a href="controlarReserva.php" type="submit" class="btn btn-warning"><i class="fa-solid fa-rotate-left"></i></i>
                         Voltar</a>
 
                 </form>
             </div>
         </div>
-        <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"
-            integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw=="
-            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js"
-            integrity="sha512-0XDfGxFliYJPFrideYOoxdgNIvrwGTLnmK20xZbCAvPfLGQMzHUsaqZK8ZoH+luXGRxTrS46+Aq400nCnAT0/w=="
-            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        
         <script>
-        // Função para exibir a mensagem de confirmação
-        window.onbeforeunload = function() {
-            return "Você tem certeza que deseja sair desta página? Suas informações não serão salvas.";
-        };
-       
-        // Lógica para remover a mensagem de confirmação quando o formulário for enviado
-        document.querySelector('form').addEventListener('submit', function() {
-            window.onbeforeunload = null;
-        });
-    </script>
+            function validaForm(form) {
+                console.log("Validação do formulário acionada!");
+                var dataEntrada = new Date(document.getElementById("dataEntrada").value);
+                var dataSaida = new Date(document.getElementById("dataSaida").value);
+
+                // Verifica se as datas são válidas
+                if (isNaN(dataEntrada.getTime()) || isNaN(dataSaida.getTime())) {
+                    alert("Por favor, insira datas válidas no formato correto.");
+                    return false;
+                }
+
+                // Compara as datas
+                if (dataSaida < dataEntrada) {
+                    alert("A data de saída deve ser posterior à data de entrada.");
+                    return false; // Impede o envio do formulário
+                }
+
+                // Continue com o envio do formulário se a validação passar
+                return true;
+            }
+        </script>
+        <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js" integrity="sha512-0XDfGxFliYJPFrideYOoxdgNIvrwGTLnmK20xZbCAvPfLGQMzHUsaqZK8ZoH+luXGRxTrS46+Aq400nCnAT0/w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+        <script>
+            // Função para exibir a mensagem de confirmação
+            window.onbeforeunload = function() {
+                return "Você tem certeza que deseja sair desta página? Suas informações não serão salvas.";
+            };
+
+            // Lógica para remover a mensagem de confirmação quando o formulário for enviado
+            document.querySelector('form').addEventListener('submit', function() {
+                window.onbeforeunload = null;
+            });
+        </script>
 </body>
 
 </html>
--- listarQuarto.php
+++ listarQuarto.php
@@ -26,7 +26,26 @@ $resultado = mysqli_query($conexao, $sql);
 ?>
 
 <?php require_once("cabecalho.php"); ?>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="barbara.ico" type="image/x-icon">
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/b78968e6be.js" crossorigin="anonymous"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Josefin+Slab:ital@1&family=Sorts+Mill+Goudy&family=Unna:ital@1&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css"
+        integrity="sha384-QYIZto+st3yW+o8+5OHfT6S482Zsvz2WfOzpFSXMF9zqeLcFV0/wlZpMtyFcZALm" crossorigin="anonymous">
+   <link href="css/style.css" rel="stylesheet">
 <div class="container">
 <!--Bloco de mensagem-->
 <?php if (isset($mensagem)) { ?>
